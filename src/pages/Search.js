4b90dc436bf674efe1d77bb5fd4a62fd6b0052f4
himanshuyadav14 <Himanshu1156044@gmail.com>
2025-02-10 23:19:11 +0530

    search removed

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,6 @@
 import SearchResult from "../components/SearchResult";
 import { useEffect, useState } from "react";
-import GptSearchBar from "../components/GptSearchBar";
+// import GptSearchBar from "../components/GptSearchBar";
 
 const Search = () => {
   const [skinOpacity, setSkinOpacity] = useState(1);
@@ -37,7 +37,7 @@ const Search = () => {
         style={{ opacity: `${skinOpacity}` }}
       ></div>
       <div className="search relative">
-        <GptSearchBar searchOpacity={searchOpacity} />
+        {/* <GptSearchBar searchOpacity={searchOpacity} /> */}
         <div className="mt-3 px-4 md:px-12 py-3 min-h-[700px]">
           <SearchResult />
         </div>
