0a79142975c3ede67a5a35ffc732146b60be1502
himanshuyadav14 <Himanshu1156044@gmail.com>
2025-02-12 12:28:28 +0530

    remove youtube branding from homepage trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,21 +9,34 @@ const VideoBackground = ({ videos, title, backdrop, poster }) => {
 
   return (
     <div className="absolute w-full h-full">
-      <img className="h-full w-full object-cover md:hidden" src={`${TMDB_CDN_URL}/w500${poster}`} alt={title} />
-      <img className={`h-full w-full object-cover hidden md:block ${trailerKey && 'xl:hidden'}`} src={`${TMDB_CDN_URL}/w1280${backdrop}`} alt={title} />
-      {trailerKey &&
-        <div className="w-full h-full hidden xl:block">
-          <iframe className="w-full object-cover aspect-video"
-            src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&mute=${isMuted ? 1 : 0}&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1`}
+      <img
+        className="h-full w-full object-cover md:hidden"
+        src={`${TMDB_CDN_URL}/w500${poster}`}
+        alt={title}
+      />
+      <img
+        className={`h-full w-full object-cover hidden md:block ${
+          trailerKey && "xl:hidden"
+        }`}
+        src={`${TMDB_CDN_URL}/w1280${backdrop}`}
+        alt={title}
+      />
+      {trailerKey && (
+        <div className="w-[300%] h-full hidden xl:block ml-[-100%]">
+          <iframe
+            className="w-full h-full object-cover"
+            src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&mute=${
+              isMuted ? 1 : 0
+            }&loop=1&controls=0&color=white&rel=0&iv_load_policy=3&modestbranding=1&fs=0&disablekb=1&playsinline=1&playlist=${trailerKey}`}
             title="YouTube video player"
             loading="lazy"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen>
-          </iframe>
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          ></iframe>
         </div>
-      }
-    </div >
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
